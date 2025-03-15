a4b0c1f8367f58b8fb6bf6035579b42d2f69dcc0
Bruna Rocha <bsant4131@gmail.com>
2025-03-15 15:17:04 -0300

    Adicionando as funções para ler as informações dadas pelo usuário.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 import os
 from datetime import datetime
-# from entrada_dados import obter_data, obter_quantia, obter_categoria, obter_descricao
+from entrada_dados import obter_data, obter_quantia, obter_categoria, obter_descricao
 
 class CSV:
     ARQUIVO_CSV ="dados_financeiros.csv"
@@ -16,43 +16,25 @@ class CSV:
             df = pd.DataFrame(columns=cls.COLUNAS)
             df.to_csv(cls.ARQUIVO_CSV, index=False)
 
+    @classmethod
+    def add_entrada(cls, data, quantia, categoria, descricao):
+            nova_entrada = {
+                "data": data,
+                "quantia": quantia,
+                "categoria": categoria,
+                "descricao": descricao
+            }
+            with open(cls.ARQUIVO_CSV, "a", newline="") as csvfile:
+                 escritos = csv.DictWriter(csvfile, fieldnames= cls.COLUNAS )
+                 escritos.writerow(nova_entrada)
+            print("Entrada adicionada com sucesso!")
+
+def add():
+     CSV.initialize_csv()
+     data = obter_data("Informe a data da transação (dd-mm-aaaa)ou informe a data de hoje: ", permitir_padrao = True)
+     quantia = obter_quantia()
+     categoria = obter_categoria()
+     descricao = obter_descricao()
+     CSV.add_entada(data, quantia, categoria, descricao)
+
 CSV.initialize_csv()
-
-
-# class CSV:
-#     ARQUIVO_CSV ="dados_financeiros.csv"
-#     COLUNAS = ["data", "quantia", "categoria", "descricao"]
-
-# # inicializa o arquivo csv
-#     @classmethod
-#     def initialize_csv(cls):
-#         try:
-#             pd.read_csv(cls.ARQUIVO_CSV)
-#         except FileNotFoundError:
-#             df = pd.DataFrame(columns=cls.COLUNAS)
-#             df.to_csv(cls.ARQUIVO_CSV, index=False)
-
-# # adiciona dados ao arquivo csv
-#     @classmethod
-#     def add_entrada(cls, data, quantia, categoria, descricao):
-#             nova_entrada = {
-#                 "data": data,
-#                 "quantia": quantia,
-#                 "categoria": categoria,
-#                 "descricao": descricao
-#             }
-#             with open(cls.ARQUIVO_CSV, "a", newline="") as csvfile:
-#                  escritos = csv.DictWriter(csvfile, fieldnames= cls.COLUNAS )
-#                  escritos.writerow(nova_entrada)
-#             print("Entrada adicionada com sucesso!")
-
-# def add():
-#      CSV.initialize_csv()
-#      data = obter_data("Informe a data da transação (dd-mm-aaaa)ou informe a data de hoje: ", permitir_padrao = True)
-#      quantia = obter_quantia()
-#      categoria = obter_categoria()
-#      descricao = obter_descricao()
-#      CSV.add_entada(data, quantia, categoria, descricao)
-
-# CSV.initialize_csv()
-
